6d1b5313ab16f4200e707186774e110c385a0745
Subhodeep Kumar Singh <subhodeep2791999@gmail.com>
2024-09-10 01:14:10 +0530

    used ModelSerializer

--- drfProj1/drfApp/api/serializers.py
+++ drfProj1/drfApp/api/serializers.py
@@ -1,36 +1,54 @@
 from rest_framework import serializers
 from drfApp.models import Movie
 
-#using validators=[]
-def name_length(value):
-    if len(value)<2:
-        raise serializers.ValidationError("Name too short")
-    return value
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Movie
+        fields="__all__"
+        #exclude=["rating"]
 
-class MovieSerializer(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField(validators=[name_length])
-    rating=serializers.FloatField()
-    description=serializers.CharField()
 
-    def create(self,validated_data):
-        return Movie.objects.create(**validated_data)
-    
-    def update(self,instance,validated_data):
-        instance.name=validated_data.get("name",instance.name)
-        instance.rating=validated_data.get("rating",instance.rating)
-        instance.description=validated_data.get("description",instance.description)
-        instance.save()
-        return instance
-    
-    #field level validation
     def validate_rating(self,value):
         if value>10:
             raise serializers.ValidationError("Max rating can be 10")
         return value
     
-    #object level validation
     def validate(self, data):
         if data['name']==data['description']:
             raise serializers.ValidationError("Description cannot be same as name")
         return data
+
+
+
+# #using validators=[] core argument
+# def name_length(value):
+#     if len(value)<2:
+#         raise serializers.ValidationError("Name too short")
+
+# class MovieSerializer(serializers.Serializer):
+#     id=serializers.IntegerField(read_only=True)
+#     name=serializers.CharField(validators=[name_length])
+#     rating=serializers.FloatField()
+#     description=serializers.CharField()
+
+#     def create(self,validated_data):
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self,instance,validated_data):
+#         instance.name=validated_data.get("name",instance.name)
+#         instance.rating=validated_data.get("rating",instance.rating)
+#         instance.description=validated_data.get("description",instance.description)
+#         instance.save()
+#         return instance
+    
+#     #field level validation
+#     def validate_rating(self,value):
+#         if value>10:
+#             raise serializers.ValidationError("Max rating can be 10")
+#         return value
+    
+#     #object level validation
+#     def validate(self, data):
+#         if data['name']==data['description']:
+#             raise serializers.ValidationError("Description cannot be same as name")
+#         return data
